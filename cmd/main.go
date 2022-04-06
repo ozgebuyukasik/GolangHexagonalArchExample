911a5b6fed502cf898cb6d9151bf26e7f6d56774
ozgeb <ozge.bykask@outlook.com>
2022-04-06 15:12:44 +0300

    define arithmetic adapter struct and implement Arithmetic port interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"hexArch/internal/adapters/core/arithmetic"
+)
 
+func main() {
+	arithmeticAdapter := arithmetic.NewAdapter()
+
+	result, err := arithmeticAdapter.Addition(1, 3)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
 }
---
+++ internal/adapters/core/arithmetic/arithmetic.go
@@ -0,0 +1,23 @@
+package arithmetic
+
+type Adapter struct {
+}
+
+func NewAdapter() *Adapter {
+	return &Adapter{}
+}
+
+func (arithmeticAdapter Adapter) Addition(operandOne int32, operandTwo int32) (int32, error) {
+	return operandOne + operandTwo, nil
+}
+
+func (artihmeticAdapter Adapter) Substraction(operandOne int32, operandTwo int32) (int32, error) {
+	return operandOne - operandTwo, nil
+}
+func (arithmeticAdapter Adapter) Multiplication(operandOne int32, operandTwo int32) (int32, error) {
+	return operandOne * operandTwo, nil
+}
+
+func (artihmeticAdapter Adapter) Division(operandOne int32, operandTwo int32) (int32, error) {
+	return operandOne / operandTwo, nil
+}
